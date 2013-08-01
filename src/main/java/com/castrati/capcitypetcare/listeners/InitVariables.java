d73c940a23bf108727e2f3b45879c969b8db67ee
Anthony Castrati <anthonyvcastrati@gmail.com>
2013-07-31 23:01:16 -0400

    Updated to Bootstrap 3.  Menu is broken for mobile versions. Form validation complete for contacts page. Added font-awesome

---
+++ src/main/java/com/castrati/capcitypetcare/listeners/InitVariables.java
@@ -0,0 +1,38 @@
+package com.castrati.capcitypetcare.listeners;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class InitVariables implements ServletContextListener
+{
+
+   @Override
+   public void contextDestroyed(final ServletContextEvent event)
+   {
+   }
+
+   @Override
+   public void contextInitialized(final ServletContextEvent event)
+   {
+      final String PROPS = "/WEB-INF/.properties";
+      final Properties propsFromFile = new Properties();
+      try
+      {
+         propsFromFile.load(getClass().getResourceAsStream(PROPS));
+      }
+      catch (final IOException e)
+      {
+          // can't get resource
+      }
+      for (String prop : propsFromFile.stringPropertyNames())
+      {
+         if (System.getProperty(prop) == null)
+         {
+             System.setProperty(prop, propsFromFile.getProperty(prop));
+         }
+      }
+   }
+}  
