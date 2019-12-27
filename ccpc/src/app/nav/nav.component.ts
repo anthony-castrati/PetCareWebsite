6290985c34df82b15e8e92fd9e41966f8044d1d8
Anthony Castrati <anthonyvcastrati@gmail.com>
2019-12-26 21:09:14 -0500

    Navigation - close on link click and style fixes for mobile

--- ccpc/src/app/nav/nav.component.ts
+++ ccpc/src/app/nav/nav.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.sass']
 })
-export class NavComponent implements OnInit {
+export class NavComponent {
+  @ViewChild('navbar-toggler', {static: true}) navbarToggler: ElementRef;
 
   constructor() { }
 
-  ngOnInit() {
+  navBarTogglerIsVisible() {
+    return this.navbarToggler.nativeElement.offsetParent !== null;
   }
 
+  collapseNav() {
+    if (this.navBarTogglerIsVisible()) {
+      this.navbarToggler.nativeElement.click();
+    }
+  }
 }
